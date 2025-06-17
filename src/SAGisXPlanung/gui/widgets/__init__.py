4c170d87f6d19836b6b4bfdb2e6c7078942e7f5d
Jakob Wilhelm <wilhelm@nti.biz>
2025-06-17 16:24:29 +0200

    remove unused widget

--- src/SAGisXPlanung/gui/widgets/__init__.py
+++ src/SAGisXPlanung/gui/widgets/__init__.py
@@ -2,6 +2,5 @@ from .template_edit import QBuildingTemplateEdit
 from .po_styling_options import QCommonStylingOptions
 from .parish_edit import QParishEdit, QParishLabel
 from .svg_symbol_edit import SVGSymbolDisplayWidget
-from .selected_geoms_view import QSelectedGeometriesView
 from .commons import ElideLabel
 from .settings import (QAttributeConfigView, AttributeConfigPage, DatabaseConfigPage)
