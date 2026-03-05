52fbe33bb41c64d099678dc638be54d3694420f3
Jakob Wilhelm <wilhelm@nti.biz>
2026-03-05 15:37:35 +0100

    add new object type BP_WasserwirtschaftsFlaeche
    (cherry picked from commit e578cd2242e7ec28726cc5c069d8ae332f26b97a)

--- src/SAGisXPlanung/BPlan/BP_Wasser/feature_types.py
+++ src/SAGisXPlanung/BPlan/BP_Wasser/feature_types.py
@@ -1,4 +1,5 @@
-from qgis.core import (QgsSymbol, QgsWkbTypes, QgsSymbolLayerUtils, QgsSimpleFillSymbolLayer)
+from qgis.PyQt.QtCore import Qt
+from qgis.core import (QgsSymbol, QgsWkbTypes, QgsSymbolLayerUtils, QgsSimpleFillSymbolLayer, QgsSimpleLineSymbolLayer, QgsUnitTypes)
 from qgis.PyQt.QtGui import QColor
 from qgis.PyQt.QtCore import QSize
 
@@ -8,15 +9,15 @@ from SAGisXPlanung import XPlanVersion
 from SAGisXPlanung.BPlan.BP_Basisobjekte.feature_types import BP_Objekt
 from SAGisXPlanung.RuleBasedSymbolRenderer import RuleBasedSymbolRenderer
 from SAGisXPlanung.XPlan.core import xp_version
-from SAGisXPlanung.XPlan.renderer import fallback_renderer
-from SAGisXPlanung.XPlan.enums import XP_ZweckbestimmungGewaesser
+from SAGisXPlanung.XPlan.renderer import fallback_renderer, icon_renderer
+from SAGisXPlanung.XPlan.enums import XP_ZweckbestimmungGewaesser, XP_ZweckbestimmungWasserwirtschaft
 from SAGisXPlanung.core.mixins.mixins import PolygonGeometry, FlaechenschlussObjekt
-from SAGisXPlanung.XPlan.types import GeometryType
+from SAGisXPlanung.XPlan.types import GeometryType, XPEnum
 
 
 @xp_version(versions=[XPlanVersion.FIVE_THREE])
 class BP_GewaesserFlaeche(PolygonGeometry, FlaechenschlussObjekt, BP_Objekt):
-    """ Festsetzungen von öffentlichen und privaten Grünflächen (§ 9, Abs. 1, Nr. 15 BauGB)."""
+    """ Festsetzung neuer Wasserflächen nach §9 Abs. 1 Nr. 16a BauGB. """
 
     __tablename__ = 'bp_gewaesser'
     __mapper_args__ = {
@@ -58,3 +59,45 @@ class BP_GewaesserFlaeche(PolygonGeometry, FlaechenschlussObjekt, BP_Objekt):
     @classmethod
     def previewIcon(cls):
         return QgsSymbolLayerUtils.symbolPreviewIcon(cls.symbol(), QSize(16, 16))
+
+
+@xp_version(versions=[XPlanVersion.FIVE_THREE])
+class BP_WasserwirtschaftsFlaeche(PolygonGeometry, FlaechenschlussObjekt, BP_Objekt):
+    """
+    Flächen für die Wasserwirtschaft (§9 Abs. 1 Nr. 16a BauGB), sowie Flächen für Hochwasserschutz-anlagen und
+    für die Regelung des Wasserabflusses (§9 Abs. 1 Nr. 16b BauGB).
+    """
+
+    __tablename__ = 'bp_wasserwirtschaft'
+    __mapper_args__ = {
+        'polymorphic_identity': 'bp_wasserwirtschaft',
+    }
+
+    id = Column(ForeignKey("bp_objekt.id", ondelete='CASCADE'), primary_key=True)
+
+    zweckbestimmung = Column(XPEnum(XP_ZweckbestimmungWasserwirtschaft, include_default=True))
+
+    @classmethod
+    def polygon_symbol(cls) -> QgsSymbol:
+        symbol = QgsSymbol.defaultSymbol(QgsWkbTypes.GeometryType.PolygonGeometry)
+        symbol.deleteSymbolLayer(0)
+
+        fill = QgsSimpleFillSymbolLayer(QColor('#ffffff'))
+        symbol.appendSymbolLayer(fill)
+
+        blue_strip = QgsSimpleLineSymbolLayer(QColor('#45a1d0'))
+        blue_strip.setWidth(5)
+        blue_strip.setOffset(2.5)
+        blue_strip.setOutputUnit(QgsUnitTypes.RenderUnit.RenderMapUnits)
+        blue_strip.setPenJoinStyle(Qt.PenJoinStyle.MiterJoin)
+        symbol.appendSymbolLayer(blue_strip)
+
+        return symbol
+
+    @classmethod
+    @fallback_renderer
+    def renderer(cls, geom_type: GeometryType = None):
+        if geom_type == QgsWkbTypes.GeometryType.PolygonGeometry:
+            return icon_renderer('Wasserwirtschaft', cls.polygon_symbol(),
+                                 'BP_Wasser', geometry_type=geom_type)
+        raise Exception('parameter geometryType should only be PolygonGeometry, but is: ' + str(geom_type))
--- src/SAGisXPlanung/utils.py
+++ src/SAGisXPlanung/utils.py
@@ -42,7 +42,7 @@ from SAGisXPlanung.BPlan.BP_Ver_und_Entsorgung.data_types import BP_KomplexeZwec
 from SAGisXPlanung.BPlan.BP_Ver_und_Entsorgung.feature_types import BP_VerEntsorgung
 from SAGisXPlanung.BPlan.BP_Verkehr.feature_types import BP_StrassenVerkehrsFlaeche, BP_StrassenbegrenzungsLinie, \
     BP_VerkehrsflaecheBesondererZweckbestimmung, BP_BereichOhneEinAusfahrtLinie, BP_EinfahrtPunkt
-from SAGisXPlanung.BPlan.BP_Wasser.feature_types import BP_GewaesserFlaeche
+from SAGisXPlanung.BPlan.BP_Wasser.feature_types import BP_GewaesserFlaeche, BP_WasserwirtschaftsFlaeche
 from SAGisXPlanung.FPlan.FP_Aufschuettung_Abgrabung_Bodenschaetze.feature_types import FP_Abgrabung, FP_Aufschuettung
 from SAGisXPlanung.FPlan.FP_Basisobjekte.codelists import FP_SonstPlanArt, FP_Status
 from SAGisXPlanung.FPlan.FP_Basisobjekte.feature_types import FP_Plan, FP_Bereich, FP_Objekt, FP_TextAbschnitt
@@ -184,6 +184,7 @@ CLASSES = {
     'BP_TextAbschnitt': BP_TextAbschnitt,
     'BP_NebenanlagenAusschlussFlaeche': BP_NebenanlagenAusschlussFlaeche,
     'BP_WohngebaeudeFlaeche': BP_WohngebaeudeFlaeche,
+    'BP_WasserwirtschaftsFlaeche': BP_WasserwirtschaftsFlaeche,
 
     'FP_Objekt': FP_Objekt,
     'FP_GenerischesObjekt': FP_GenerischesObjekt,
